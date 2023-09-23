b0edbc39188cfd0ef8979639f6662b5a0596bc65
chastycaacoy <caacoy.chastymaeg.10022002@gmail.com>
2023-09-23 12:52:12 +0800

    updated names

---
+++ IPT102-asp-starter-main/Controllers/HomeController.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+public class HomeController : Controller
+{
+    public IActionResult Index()
+    {
+        var studentInfoArray = new[]
+        {
+            new StudentInfoModel { Name = "Chasty Mae Caacoy", StudentId = "211467", StudentAge = "20" },
+            new StudentInfoModel { Name = "Vergilia Caacoy", StudentId = "211567", StudentAge = "30" },
+            new StudentInfoModel { Name = "Domingo Caacoy", StudentId = "211667", StudentAge = "40" }
+        };
+
+        return View(studentInfoArray);
+    }
+}
