a72cb71281a67d9639effeb3ed246f2aece10faf
chastycaacoy <caacoy.chastymaeg.10022002@gmail.com>
2023-09-30 09:47:48 +0800

    updated infofo

---
+++ Controllers/HomeController.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+public class HomeController : Controller
+{
+    public IActionResult Index()
+    {
+        var studentInfoArray = new[]
+        {
+            new StudentInfoModel { Name = "Chasty caacoy", StudentId = "211467", StudentAge = "20" },
+            new StudentInfoModel { Name = "Vergilia Caacoy", StudentId = "211567", StudentAge = "25" },
+            new StudentInfoModel { Name = "Domingo Caacoy", StudentId = "211667", StudentAge = "31" }
+        };
+
+        return View(studentInfoArray);
+    }
+}
